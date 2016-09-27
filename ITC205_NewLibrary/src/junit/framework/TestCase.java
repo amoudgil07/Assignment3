d32fc4c04f6ffbd7ae02207290d3ba3ab1f461e3
amoudgil07 <amoudgil07@gmail.com>
2016-09-27 14:14:59 +1000

    Add files via upload

---
+++ ITC205_NewLibrary/src/junit/framework/TestCase.java
@@ -0,0 +1,25 @@
+package junit.framework;
+
+public class TestCase {
+
+	protected void setUp() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUp1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUp2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setUp3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
